f1126084370a04ad7d52ca3fb44023657074bb2e
Neri Yarden <62503534+neriyarden@users.noreply.github.com>
2023-09-27 22:55:04 +0300

    setup ui-components (#1)

--- apps/honfolio/src/app/page.tsx
+++ apps/honfolio/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'My Honfolio',
+}
+ 
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return <h1 className="text-red-800">Hello, Next.js!</h1>
 }
