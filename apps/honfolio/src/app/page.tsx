970de2d4367c0ab31a990bddd6b03727a4941c9a
neriyarden <neriyarden@gmail.com>
2023-11-18 18:12:20 +0200

    remove import from ui-components in app

--- apps/honfolio/src/app/page.tsx
+++ apps/honfolio/src/app/page.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import { DemoButton } from "@neri/ui-components";
 
 export const metadata: Metadata = {
   title: "My Honfolio",
 };
 
 export default function Page() {
-  return (
-    <>
-      <h1 className="text-red-800">Hello, Next.js!</h1>
-      <DemoButton>my button</DemoButton>
-    </>
-  );
+  return <h1 className="text-red-800">Hello, Next.js!</h1>;
 }
