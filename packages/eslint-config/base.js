a0181071b4ebbebdeb3abb7d1609831c6b9f29d4
Neri Yarden <62503534+neriyarden@users.noreply.github.com>
2023-11-25 14:57:34 +0200

    Infra/add eslint rules (#4)
    * infra/add lint and test ci jobs
    
    * tsconfig fix
    
    * eslint added rules
    
    * add typecheck ci step
    
    * rename ci job
    
    * fix lint error
    
    * fix tests error

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -4,6 +4,7 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 module.exports = {
   extends: ["eslint-config-turbo"].map(require.resolve),
+  plugins: ["simple-import-sort"],
   parserOptions: {
     project,
   },
@@ -22,5 +23,28 @@ module.exports = {
     "@typescript-eslint/consistent-type-definitions": "off",
     "unicorn/filename-case": "off",
     "no-console": "warn",
+    "simple-import-sort/exports": "error",
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          // Packages. react related packages come first.
+          ["^react", "^node", "^@?\\w"],
+          // "@neri/" shared packages.
+          ["^@neri/.*"],
+          // Side effects imports.
+          ["\\u0000"],
+          // "@/" internal imports
+          ["^@/.*"],
+          // Parent imports. Put `..` last.
+          [("^\\.\\.(?!/?$)", "^\\.\\./?$")],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          // Style imports.
+          ["^.+\\.s?css$"],
+        ],
+      },
+    ],
+    "import/order": "off",
   },
 };
